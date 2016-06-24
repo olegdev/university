914d8186172e42593254b961c71c14b3121dcc93
Mac <mac@1-93.2reallife.net>
2016-06-24 15:48:11 +0300

    Иконки в легенде в виде пятиугольников

--- server/services/profiles.js
+++ server/services/profiles.js
@@ -8,107 +8,116 @@ module.exports = {
 			title: 'Shopping',
 			color: 'blue',
 			required: true,
+			order: 0
 		},
 		{
 			type: 'professional',
 			title: 'Professional',
 			color: 'green',
 			required: true,
+			order: 1
 		},
 		{
 			type: 'health',
 			title: 'Health',
 			color: '#ff8391',
+			order: 2
 		},
 		{
 			type: 'food',
 			title: 'Food',
 			color: '#0938ab',
+			order: 3
 		},
 		{
 			type: 'family',
 			title: 'Family',
 			color: '#f5f7f0',
+			order: 4
 		},
 		{
 			type: 'friends',
 			title: 'Friends',
 			color: '#9386af',
+			order: 5
 		},
 		{
 			type: 'education',
 			title: 'Education',
 			color: '#629255',
+			order: 6
 		},
 		{
 			type: 'spirituality',
 			title: 'Spirituality',
 			color: '#262003',
+			order: 7
 		},
 		{
 			type: 'travel',
 			title: 'Travel',
 			color: '#82f930',
+			order: 8
 		},
 		{
 			type: 'music',
 			title: 'Music',
 			color: '#faf390',
+			order: 9
 		},
 		{
 			type: 'video',
 			title: 'Video',
 			color: '#629255',
+			order: 10
 		},
 		{
 			type: 'finance',
 			title: 'Finance',
 			color: '#f7c733',
+			order: 11
 		},
 		{
 			type: 'dating',
 			title: 'Dating',
 			color: '#5209ff',
+			order: 12
 		},
 		{
 			type: 'nature',
 			title: 'Nature',
 			color: '#cc77ae',
+			order: 13
 		},
 		{
 			type: 'gaming',
 			title: 'Gaming',
 			color: '#90cef4',
+			order: 14
 		},
 		{
 			type: 'charity',
 			title: 'Charity',
 			color: '#efedea',
+			order: 15
 		},
 		{
 			type: 'pet',
 			title: 'Pet',
 			color: 'red',
+			order: 16
 		},
 		{
 			type: 'books',
 			title: 'Books',
 			color: 'yellow',
+			order: 17
 		},
 		{
 			type: 'hobby',
 			title: 'Hobbies',
 			color: 'grey',
-		},
-		{
-			type: 'design',
-			title: 'Design',
-			color: 'brown',
-		},
-		{
-			type: 'sport',
-			title: 'Sport',
-			color: 'black',
+			order: 18
 		},
 	],
 
